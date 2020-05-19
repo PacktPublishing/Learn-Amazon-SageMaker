abd902f6ab63625088f701a90c83f38db89b9cab
Julien Simon <ec2-user@ip-172-16-57-65.eu-west-1.compute.internal>
2020-05-19 09:33:11 +0000

    Initial version

---
+++ ch8/sklearn/sklearn-boston-housing-generic.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+import joblib
+import os, json
+
+if __name__ == '__main__':
+    
+    config_dir = '/opt/ml/input/config'
+    training_dir = '/opt/ml/input/data/training'
+    model_dir = '/opt/ml/model'
+    
+    with open(os.path.join(config_dir, 'hyperparameters.json')) as f:
+        hp = json.load(f)
+        print(hp)
+        normalize = hp['normalize']
+        test_size = float(hp['test-size'])
+        random_state = int(hp['random-state'])
+        
+    filename = os.path.join(training_dir, 'housing.csv')
+    data = pd.read_csv(filename, delim_whitespace=True)
+    labels = data[['medv']]
+    samples = data.drop(['medv'], axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(samples, labels, 
+                                                        test_size=test_size, random_state=random_state)
+    regr = LinearRegression(normalize=normalize)
+    regr.fit(X_train, y_train)
+    y_pred = regr.predict(X_test)
+    print('Mean squared error: %.2f' % mean_squared_error(y_test, y_pred))
+    print('Coefficient of determination: %.2f' % r2_score(y_test, y_pred))
+    
+    joblib.dump(regr, os.path.join(model_dir, 'model.joblib'))
