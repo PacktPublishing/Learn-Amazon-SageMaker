91aade0543737334634385775458ff2a6449ac94
Julien Simon <ec2-user@ip-172-16-57-65.eu-west-1.compute.internal>
2020-05-19 15:16:47 +0000

    Cleanup

--- ch8/R/train.R
+++ ch8/R/train.R
@@ -1,4 +1,3 @@
-install.packages("rjson")
 library("rjson")
 
 hp <- fromJSON(file = '/opt/ml/input/config/hyperparameters.json')
@@ -9,9 +8,9 @@ data <- read.table(file = '/opt/ml/input/data/training/housing.csv', header=T)
 if (normalize) {
     data <- as.data.frame(scale(data))
 }
-summary(data)
+print(summary(data))
 
 model = lm(medv~.,data)
-summary(model)
+print(summary(model))
 
 saveRDS(model, '/opt/ml/model/model.rds')
